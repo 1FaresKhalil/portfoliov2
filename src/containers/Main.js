a526cd80e5f1903bfe4f478fa1bcf57c75d00418
1FaresKhalil <cds.faresmohamad15347@alexu.edu.eg>
2021-10-27 17:40:27 +0200

    commit

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -10,7 +10,7 @@ import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
 import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
+// import Podcast from "./podcast/Podcast";
 import Education from "./education/Education";
 import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
@@ -42,7 +42,7 @@ const Main = () => {
         <Blogs />
         <Talks />
         <Twitter />
-        <Podcast />
+        {/* <Podcast /> */}
         <Profile />
         <Footer />
         <Top />
